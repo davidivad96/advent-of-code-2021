e414b4bc9e9cbca2293c6ac32dd521ddc40c696e
davidivad96 <davidivad96@gmail.com>
2021-12-08 19:36:12 +0100

    Solve part 2 of problem 8

--- 8/index.js
+++ 8/index.js
@@ -22,8 +22,60 @@ const solve1 = (patterns, outputs) => {
   }, 0)
 }
 
+const numberOfCharactersIncluded = (string, characters) => {
+  let sum = 0
+  for (const character of characters) {
+    if (string.includes(character)) {
+      sum++
+    }
+  }
+  return sum
+}
+
+const calculateNumbers = (pattern) => {
+  let pattern_copy = [...pattern]
+  const one = pattern_copy.find(segments => segments.length === 2)
+  pattern_copy.splice(pattern_copy.indexOf(one), 1)
+  const four = pattern_copy.find(segments => segments.length === 4)
+  pattern_copy.splice(pattern_copy.indexOf(four), 1)
+  const seven = pattern_copy.find(segments => segments.length === 3)
+  pattern_copy.splice(pattern_copy.indexOf(seven), 1)
+  const eight = pattern_copy.find(segments => segments.length === 7)
+  pattern_copy.splice(pattern_copy.indexOf(eight), 1)
+  const nine = pattern_copy.find(segments => segments.length === 6 && numberOfCharactersIncluded(segments, four) === 4)
+  pattern_copy.splice(pattern_copy.indexOf(nine), 1)
+  const zero = pattern_copy.find(segments => segments.length === 6 && numberOfCharactersIncluded(segments, one) === 2)
+  pattern_copy.splice(pattern_copy.indexOf(zero), 1)
+  const six = pattern_copy.find(segments => segments.length === 6)
+  pattern_copy.splice(pattern_copy.indexOf(six), 1)
+  const three = pattern_copy.find(segments => segments.length === 5 && numberOfCharactersIncluded(segments, one) === 2)
+  pattern_copy.splice(pattern_copy.indexOf(three), 1)
+  const five = pattern_copy.find(segments => segments.length === 5 && numberOfCharactersIncluded(segments, four) === 3)
+  pattern_copy.splice(pattern_copy.indexOf(five), 1)
+  const two = pattern_copy[0]
+  pattern_copy.splice(pattern_copy.indexOf(two), 1)
+  return [zero, one, two, three, four, five, six, seven, eight, nine]
+}
+
+const decodeOutput = (numbers, output) => {
+  let number = ''
+  for (const digit of output) {
+    number = number.concat(
+      numbers.findIndex(
+        n => n.length === digit.length && numberOfCharactersIncluded(n, digit) === digit.length
+      )
+    )
+  }
+  return Number(number)
+}
+
 const solve2 = (patterns, outputs) => {
-  return 2
+  let sum = 0
+  for (let i = 0; i < patterns.length; i++) {
+    const numbers = calculateNumbers(patterns[i])
+    sum += decodeOutput(numbers, outputs[i])
+  }
+  return sum
 }
 
 readFile("input.txt").then(([patterns, outputs]) => {
