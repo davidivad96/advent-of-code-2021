8ea0a575c19c2857cdb0190a8483d035111a11ba
davidivad96 <davidivad96@gmail.com>
2021-12-10 10:03:24 +0100

    Solve part 1 of problem 10

---
+++ 10/index.js
@@ -0,0 +1,48 @@
+const fs = require("fs")
+const readline = require("readline")
+
+const readFile = async (path) => {
+  const fileStream = fs.createReadStream(path)
+  const rl = readline.createInterface({
+    input: fileStream,
+  })
+  const result = []
+  for await (const line of rl) {
+    result.push(line)
+  }
+  return result
+}
+
+const checkSyntax = (chunk) => {
+  const stack = []
+  const opening_characters = ['(', '[', '{', '<']
+  const closing_characters = [')', ']', '}', '>']
+  const points = {
+    [closing_characters[0]]: 3,
+    [closing_characters[1]]: 57,
+    [closing_characters[2]]: 1197,
+    [closing_characters[3]]: 25137,
+  }
+  for (const character of chunk) {
+    if (opening_characters.includes(character)) {
+      stack.push(character)
+    } else {
+      const index = closing_characters.indexOf(character)
+      if (stack.pop() !== opening_characters[index]) {
+        return points[character]
+      }
+    }
+  }
+  return 0
+}
+
+const solve1 = (input) => input.reduce((prev, curr) => prev + checkSyntax(curr), 0)
+
+const solve2 = (input) => {
+  return 2
+}
+
+readFile("input.txt").then(input => {
+  console.log(`solution to part 1: ${solve1(input)}`)
+  console.log(`solution to part 2: ${solve2(input)}`)
+})
