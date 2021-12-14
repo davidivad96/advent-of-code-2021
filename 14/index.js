b43e46ea5d63086e17178bc046276162dedd5d1a
davidivad96 <davidivad96@gmail.com>
2021-12-14 19:18:27 +0100

    Solve part 2 of problem 14

--- 14/index.js
+++ 14/index.js
@@ -25,10 +25,12 @@ const solve1 = (polymer, rules) => {
   let polymer_copy = polymer.slice();
   for (let i = 0; i < N_STEPS; i++) {
     for (let j = 0; j < polymer_copy.length; j++) {
-      if (rules[`${polymer_copy[j]}${polymer_copy[j + 1]}`]) {
-        polymer_copy = `${polymer_copy.slice(0, j + 1)}${
-          rules[`${polymer_copy[j]}${polymer_copy[j + 1]}`]
-        }${polymer_copy.slice(j + 1)}`;
+      const rule = rules[`${polymer_copy[j]}${polymer_copy[j + 1]}`];
+      if (rule) {
+        polymer_copy = `${polymer_copy.slice(
+          0,
+          j + 1
+        )}${rule}${polymer_copy.slice(j + 1)}`;
         j++;
       }
     }
@@ -46,7 +48,41 @@ const solve1 = (polymer, rules) => {
 };
 
 const solve2 = (polymer, rules) => {
-  return 2;
+  const N_STEPS = 40;
+  let pairs_count = {};
+  const characters_count = {};
+  for (let i = 0; i < polymer.length; i++) {
+    const pair = `${polymer[i]}${polymer[i + 1]}`;
+    if (rules[pair]) {
+      pairs_count[pair] = pairs_count[pair] ? pairs_count[pair] + 1 : 1;
+    }
+    const character = polymer[i];
+    characters_count[character] = characters_count[character]
+      ? characters_count[character] + 1
+      : 1;
+  }
+  for (let i = 0; i < N_STEPS; i++) {
+    const new_pairs_count = {};
+    for (const pair in pairs_count) {
+      const pair_count = pairs_count[pair];
+      const new_pair_1 = `${pair[0]}${rules[pair]}`;
+      const new_pair_2 = `${rules[pair]}${pair[1]}`;
+      new_pairs_count[new_pair_1] = new_pairs_count[new_pair_1]
+        ? new_pairs_count[new_pair_1] + pair_count
+        : pair_count;
+      new_pairs_count[new_pair_2] = new_pairs_count[new_pair_2]
+        ? new_pairs_count[new_pair_2] + pair_count
+        : pair_count;
+      characters_count[rules[pair]] = characters_count[rules[pair]]
+        ? characters_count[rules[pair]] + pair_count
+        : pair_count;
+    }
+    pairs_count = { ...new_pairs_count };
+  }
+  const sorted_count = Object.values(characters_count).sort((a, b) => a - b);
+  const least_common_character_count = sorted_count[0];
+  const most_common_character_count = sorted_count[sorted_count.length - 1];
+  return most_common_character_count - least_common_character_count;
 };
 
 readFile("input.txt").then(([polymer, rules]) => {
